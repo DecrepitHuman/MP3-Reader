22b191bd7b6066a3600a286aed70b4078be98feb
NicholasV5 <61983731+NicholasV5@users.noreply.github.com>
2020-03-10 07:52:12 +0200

    Updated to work accross multiple systems

--- reader.py
+++ reader.py
@@ -2,14 +2,14 @@ import pygame
 import tkinter as tk
 import os
 
+#Creating & Editing window:
 player = tk.Tk()
 player.title("MP3 Player")
 player.geometry("205x340")
 
 #Playlist:
-os.chdir("C:/Users/nicho/Documents/Others/Python-Projects/mp3-reader/Songs")
+os.chdir("Songs")
 songList = os.listdir()
-
 playlist = tk.Listbox(player, highlightcolor="blue", selectmode=tk.SINGLE)
 print(songList)
 for item in songList:
@@ -17,9 +17,11 @@ for item in songList:
     playlist.insert(pos, item)
     pos = pos + 1
 
+#Initiating pygame:
 pygame.init()
 pygame.mixer.init()
 
+#Functions:
 def Play():
     pygame.mixer.music.load(playlist.get(tk.ACTIVE))
     var.set(playlist.get(tk.ACTIVE))
@@ -28,15 +30,18 @@ def Play():
 def ExitPlayer():
     pygame.mixer.music.stop()
 
+#Buttons:
 Button1 = tk.Button(player, width=8, height=5, text="PLAY", command=Play)
 Button1.pack(fill="x")
 
 Button2 = tk.Button(player, width=8, height=5, text="STOP", command=ExitPlayer)
 Button2.pack(fill="x")
 
+#More Playlist code:
 var = tk.StringVar()
 songTitle = tk.Label(player, textvariable=var)
 songTitle.pack()
 playlist.pack(fill="both", expand="yes")
 
+#Starting window
 player.mainloop()
